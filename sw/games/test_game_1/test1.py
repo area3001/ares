396a02fa6fcf046bc2fc4b03f293c2451c49a7f3
Paul-Armand Verhaegen <paularmand.verhaegen@gmail.com>
2016-11-13 11:46:18 +0100

    made map edges, and updated car handing

--- sw/games/test_game_1/test1.py
+++ sw/games/test_game_1/test1.py
@@ -1,13 +1,20 @@
 import pyglet
 from pyglet.window import key
-
 import cocos
 from cocos import actions, layer, sprite, scene
 from cocos.director import director
 import cocos.euclid as eu
 import cocos.collision_model as cm
+import math
 
-MAX_VELOCITY = 150
+MAP_SIZE = (600, 600)
+
+VELOCITY_MAX = 400
+VELOCITY_INERTIA = 3  # smaller means more inertia
+VELOCITY_BRAKE_VS_SPEED = 3
+VELOCITY_IMPACT_ON_TURNING = 0.0025
+TURNING_SPEED = 5
+VELOCITY_DECLINE = 0.995  # not touching controls means the velocity will go to zero
 
 class CollidableSprite(cocos.sprite.Sprite):
   def __init__(self, image, cx, cy, radius):
@@ -24,6 +31,10 @@ class CollidableSprite(cocos.sprite.Sprite):
     if collision_manager.any_near(self, 1):
       self.velocity = (- self.velocity[0], - self.velocity[1])
 
+    # check if out of map
+    self.position = (max(0, min(self.position[0], MAP_SIZE[0])), \
+                     max(0, min(self.position[1], MAP_SIZE[1])))
+
 # How to handle collisions
 #mapcollider = mapcolliders.RectMapCollider("bounce")
 
@@ -34,13 +45,27 @@ class Car(actions.Move):
     super(Car, self).step(dt)
 
     rl = keyboard[key.RIGHT] - keyboard[key.LEFT]
-    ud = keyboard[key.UP] - keyboard[key.DOWN]
+    speed_or_brake = keyboard[key.UP] - keyboard[key.DOWN]
 
-    # need to make this from the perspective of the target
-    velocity_x = min(self.target.velocity[0] + 5 * rl, MAX_VELOCITY)
-    velocity_y = min(self.target.velocity[1] + 5 * ud, MAX_VELOCITY)
+    radians = self.target.rotation * math.pi / 180
+
+    # Update the speed from the perspective of the car
+    try:
+      speed_or_brake =   keyboard[key.UP] - VELOCITY_BRAKE_VS_SPEED * keyboard[key.DOWN] \
+                       if self.target.speed > 0 else \
+                         VELOCITY_BRAKE_VS_SPEED * keyboard[key.UP] -  keyboard[key.DOWN]
+      self.target.speed = VELOCITY_DECLINE * (min(VELOCITY_INERTIA * speed_or_brake + self.target.speed, VELOCITY_MAX))
+    except AttributeError:
+      self.target.speed = math.sqrt(self.target.velocity[0]**2 + self.target.velocity[1]**2)
+
+    velocity_x = self.target.speed * math.sin(radians)
+    velocity_y = self.target.speed * math.cos(radians)
     self.target.velocity = (velocity_x, velocity_y)
 
+    # turn the car
+    print VELOCITY_IMPACT_ON_TURNING
+    rl = TURNING_SPEED * rl * VELOCITY_IMPACT_ON_TURNING * abs(self.target.speed)
+    rl = rl if self.target.speed > 0 else - rl
     action = actions.interval_actions.RotateBy(rl, 0)
     self.target.do(action)
 
@@ -54,7 +79,7 @@ def main():
 
     collision_manager = cm.CollisionManagerBruteForce()
 
-    director.init(width=1300, height=1300, autoscale=False, resizable=True)
+    director.init(width=MAP_SIZE[0], height=MAP_SIZE[1], autoscale=True, resizable=True)
 
     # Create a layer
     player_layer = layer.Layer()
@@ -66,7 +91,7 @@ def main():
 
     # create the car and add to layer
     car = CollidableSprite('sprites/taxi.png', 100, 100, 10)
-    action = actions.interval_actions.ScaleBy(0.5, 0)
+    action = actions.interval_actions.ScaleBy(0.25, 0)
     car.do(action)
 
     player_layer.add(car)
