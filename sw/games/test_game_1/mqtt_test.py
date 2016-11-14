98cc6df699d1822acb42968b4e5d64746713ce58
Paul-Armand Verhaegen <paularmand.verhaegen@gmail.com>
2016-11-14 23:30:57 +0100

    added the mqtt test and a nicer obstacle

---
+++ sw/games/test_game_1/mqtt_test.py
@@ -0,0 +1,16 @@
+import paho.mqtt.publish as publish
+import random
+import sys
+import json
+
+ID_MAX = sys.maxint
+
+payload = {"test": 1,
+           "hide": 0,
+           "version": 1,
+           "position": [200, 400],
+           "id": str(random.randint(0, ID_MAX)),
+           "source": "real"
+           }
+
+publish.single("ares/video/markers", json.dumps(payload), hostname="localhost")
